c27d233fcd4dc1cbe227304b9e025c4404091bff
Amir123412341234 <raficovamir05@gmail.com>
2025-08-26 14:04:09 +0300

    'init'

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ int main(int argc, char* argv[]) {
          << "\t1 - add task;\n"
          << "\t2 - view file;\n"
          << "\t3 - select tasks;\n"
+         << "\t4 - select only false tasks;\n"
+         << "\t5 - mark tasks as complited;\n"
          << "\t0 - exit.\n:";
 
     cin >> action;
@@ -32,7 +34,7 @@ int main(int argc, char* argv[]) {
       FILE* binfile = fopen("bin.bin", "ab+");
       Task task;
 
-      task.number = rand();
+      task.number = rand();  // todo:исправить
 
       cout << "name: ";
       cin >> task.name;
@@ -93,6 +95,62 @@ int main(int argc, char* argv[]) {
                << tasks[i].hardLevel << "|" << tasks[i].isSolved << endl;
         }
       }
+
+      fclose(binfile);
+    } else if (action == 4) {
+      FILE* binfile = fopen("bin.bin", "rb");
+
+      if (binfile == nullptr) {
+        continue;
+      }
+
+      fseek(binfile, 0, SEEK_END);
+      long sizeBytes = ftell(binfile);
+      int countItems = sizeBytes / sizeof(Task);
+      Task tasks[100];
+
+      fseek(binfile, 0, SEEK_SET);
+      fread(&tasks[0], sizeof(Task), countItems, binfile);
+
+      for (int i = 0; i < countItems; ++i) {
+        if (tasks[i].isSolved == false) {
+          cout << tasks[i].number << "|" << tasks[i].name << "|"
+               << tasks[i].hardLevel << "|" << tasks[i].isSolved << endl;
+        }
+      }
+
+      fclose(binfile);
+    } else if (action == 5) {  // todo:чтобы изменьть 1 запись необязательно
+                               // копировать в оперативную память
+      cout << "id task is complited: ";
+      int id_edit;
+      cin >> id_edit;
+
+      FILE* binfile = fopen("bin.bin", "rb");
+
+      if (binfile == nullptr) {
+        continue;
+      }
+
+      fseek(binfile, 0, SEEK_END);
+      long sizeBytes = ftell(binfile);
+      int countItems = sizeBytes / sizeof(Task);
+      Task tasks[100];
+
+      fseek(binfile, 0, SEEK_SET);
+      fread(&tasks[0], sizeof(Task), countItems, binfile);
+
+      fclose(binfile);
+
+      binfile = fopen("bin.bin", "wb");
+
+      for (int i = 0; i < countItems; ++i) {
+        if (tasks[i].number == id_edit) {
+          tasks[i].isSolved = true;
+        }
+      }
+
+      fwrite(&tasks[0], sizeof(Task), countItems, binfile);
 
       fclose(binfile);
     }
